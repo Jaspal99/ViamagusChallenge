7d4c3ced1867874cca4e18e52e7c13dea4b660c4
Jaspal <khalsajaspalsingh6@gmail.com>
2023-10-05 13:24:39 +0530

    first commit

---
+++ App.jsx
@@ -0,0 +1,24 @@
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+function App() {
+
+  return (
+    <SafeAreaView >
+      <Text style={{fontSize:50}}>Welcome Sir</Text>
+    </SafeAreaView>
+  );
+}
+
+
+
+export default App;
